a8072a80126395a8af6a29ddbffa93afdf344fd0
anwarvic <mohamedanwarvic@gmail.com>
2021-10-29 01:55:22 +0000

    fixed the routing problem... now machine-translation works like a charm

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ exclude: 'yes'
 
 
 $(document).ready(function () {
-  // {% if site.disable_landing_page != true %}
+  {% if site.disable_landing_page != true %}
   // $('a.blog-button').click(function (e) {
   //   if ($('.panel-cover').hasClass('panel-cover--collapsed')){
   //     return
@@ -21,14 +21,14 @@ $(document).ready(function () {
   //   }
   // })
 
-  // if (window.location.hash && window.location.hash == '#blog') {
+  // if (window.location.hash && window.location.hash == 'machine-translation') {
   //   $('.panel-cover').addClass('panel-cover--collapsed')
   // }
 
-  // if (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/index.html') {
-  //   $('.panel-cover').addClass('panel-cover--collapsed')
-  // }
-  // {% endif %}
+  if (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/index.html') {
+    $('.panel-cover').addClass('panel-cover--collapsed')
+  }
+  {% endif %}
 
   $('.btn-mobile-menu').click(function () {
     $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
@@ -59,12 +59,34 @@ function expand() {
   $('.panel-cover').removeClass('panel-cover--collapsed')
 }
 
+function generateContent(id){
+  const txt = `
+  <ol class="post-list">
+  <h1>{{ page.url }}</h1>
+  {% for post in site.machine-translation%}
+    <li>
+      <h2 class="post-list__post-title post-title"><a href="{{ site.baseurl }}{{ post.url }}" title="{{ post.title }}">{{ post.title }}</a></h2>
+      <p class="excerpt">{{ post.excerpt | strip_html }}&hellip;</p>
+      <div class="post-list__meta">
+          <time datetime="{{ post.date | date: '%Y-%m-%d %H:%M' }}" class="post-list__meta--date date">{{ post.date | date: "%-d %b %Y" }}</time>
+          {% if post.tags.size > 0 %}
+          &#8226; <span class="post-meta__tags">on {% for tag in post.tags %}<a href="{{ site.baseurl }}/tags/#{{ tag }}">{{ tag }}</a>{% if forloop.last == false %}, {% endif %}{% endfor %}</span>
+          {% endif %}
+      </div>
+      <hr class="post-list__divider">
+    </li>
+  {% endfor %}
+  </ol>
+  `;
+  console.log(txt);
+  $("#list-items").html(txt);
+}
+
 function activateButton(id) {
   const elem = $('#'+id);
   // deactivate
   if (window.activeButtons.includes(id)) {
     elem.removeClass('clicked');
-    window.activeButtons.pop();
     expand();
   }
   // activate
@@ -75,5 +97,6 @@ function activateButton(id) {
     window.activeButtons.push(id);
     elem.addClass('clicked');
     collapse();
+    // generateContent(id);
   }
 }
---
+++ machine-translation.md
@@ -0,0 +1,6 @@
+---
+layout: named_collection
+collection_name: machine-translation
+title: Machine Translation
+permalink: /machine-translation/
+---
