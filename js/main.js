01505f8d94845c2ee15ba336f5ae39f1e49f403f
anwarvic <mohamedanwarvic@gmail.com>
2021-11-17 17:35:28 +0200

    removed landing page if-conidtion found in the main.js script

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ exclude: 'yes'
 
 
 $(document).ready(function () {
-  {% if site.disable_landing_page != true %}
   // $('a.blog-button').click(function (e) {
   //   if ($('.panel-cover').hasClass('panel-cover--collapsed')){
   //     return
@@ -21,16 +20,11 @@ $(document).ready(function () {
   //   }
   // })
 
-  // if (window.location.hash && window.location.hash == 'machine-translation') {
-  //   $('.panel-cover').addClass('panel-cover--collapsed')
-  // }
-
   if (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/index.html') {
     $('.panel-cover').addClass('panel-cover--collapsed')
     let id = window.location.pathname.split('/')[1];
     activateButton(id);
   }
-  {% endif %}
 
   $('.btn-mobile-menu').click(function () {
     $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
@@ -44,27 +38,26 @@ $(document).ready(function () {
 
 })
 
-// only zero or one button should be active
-window.activeButtons = [];
-const maxwidth = 2000;
-
 function collapse() {
   currentWidth = $('.panel-cover').width()
-    if (currentWidth < 960) {
-      $('.panel-cover').addClass('panel-cover--collapsed')
-      // $('.content-wrapper').addClass('animated slideInRight')
-    } else {
-      $('.panel-cover').css('max-width', currentWidth)
-      $('.panel-cover').animate({ 'max-width': '530px', 'width': '40%' }, 400, swing = 'swing', function () { })
-    }
+  if (currentWidth < 960) {
+    $('.panel-cover').addClass('panel-cover--collapsed')
+    // $('.content-wrapper').addClass('animated slideInRight')
+  } else {
+    $('.panel-cover').css('max-width', currentWidth)
+    $('.panel-cover').animate({ 'max-width': '530px', 'width': '40%' }, 400, swing = 'swing', function () { })
+  }
 }
 
+// const maxwidth = 2000;
 // function expand() {
-//   $('.panel-cover').css('max-width', maxwidth)
-//   $('.panel-cover').animate({ 'max-width': maxwidth, 'width': '100%' }, 500, swing = 'swing', function () { })
-//   $('.panel-cover').removeClass('panel-cover--collapsed')
-// }
-
+  //   $('.panel-cover').css('max-width', maxwidth)
+  //   $('.panel-cover').animate({ 'max-width': maxwidth, 'width': '100%' }, 500, swing = 'swing', function () { })
+  //   $('.panel-cover').removeClass('panel-cover--collapsed')
+  // }
+  
+// only zero or one button should be active
+window.activeButtons = [];
 function activateButton(id) {
   const elem = $('#'+id);
   // deactivate
